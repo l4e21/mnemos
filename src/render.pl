e56a532d110d9cd847d33d9bbd4d5c6f5d3238fb
Jamie Walkerdine <jwalkerdine21@hotmail.com>
2025-02-04 15:40:11 +0000

    Added depth to docs

--- src/render.pl
+++ src/render.pl
@@ -7,6 +7,9 @@ style(text, _{}).
 style(quote, _{implements:text, fontstyles:[italics]}).
 style(img, _{}).
 
+enclose_in_tags(Tag, Depth, Content, Result) :-
+    format(string(Result), "~*c<~w>\n~w~*c</~w>\n", [Depth, 32, Tag, Content, Depth, 32, Tag]).
+
 %% Priority: Meta > Style Predicate > Context
 css_style_atom(fontsize-V, "font-size", V).
 css_style_atom(color-V, "color", V).
@@ -33,60 +36,62 @@ render_as_css(N, StyleOpts, CSS) :-
 html_header_aux(StyleList, StyleString) :-
     foldl(string_concat, StyleList, "", StyleString).
     
-html_header(Header) :-
+html_header(HtmlHeader) :-
     findall(CSS, (style(N, StyleOpts), render_as_css(N, StyleOpts, CSS)), StyleList),
     write(StyleList),
     html_header_aux(StyleList, StyleString),
-    format(string(Header),
-           "<html>\n<head>\n<style>\n~w</style>\n</head>\n<body>\n",
-           [StyleString]).
+    enclose_in_tags("style", 0, StyleString, HtmlHeader).
 
 %% Main entry for rendering notes
 render(Name, Html, CtxStyleOpts) :-
     book(Name, Meta),
     notes(Name, Nodes),
     style_opts(Name, Meta, CtxStyleOpts, StyleOpts),
-    render(Nodes, HtmlBody, StyleOpts),
-    html_header(S),
-    format(string(Html), "~w~w</body\n</html>", [S, HtmlBody]).
+    render(Nodes, HtmlBody, StyleOpts, 0),
+    html_header(HtmlHeader),
+    format(string(Html), "<html>\n<head>\n~w</head>\n<body>\n~w</body>\n</html>",
+           [HtmlHeader, HtmlBody]).
 
 %% Specific Nodes
-render(h1(S, Meta), Html, CtxStyleOpts) :-
+render(h1(S, Meta), Html, CtxStyleOpts, Depth) :-
     style_opts(h1, Meta, CtxStyleOpts, StyleOpts),
-    render(S, SubHtml, StyleOpts),
-    format(string(Html), "<h1>\n~w</h1>\n", [SubHtml]). 
+    Depth1 is Depth+1,
+    render(S, SubHtml, StyleOpts, Depth1),
+    enclose_in_tags("h1", Depth, SubHtml, Html).
 
-render(h1(S), Html, CtxStyleOpts) :-
-    render(h1(S, _{}), Html, CtxStyleOpts).
+render(h1(S), Html, CtxStyleOpts, Depth) :-
+    render(h1(S, _{}), Html, CtxStyleOpts, Depth).
 
-render(text(S, Meta), Html, CtxStyleOpts) :-
+render(text(S, Meta), Html, CtxStyleOpts, Depth) :-
     style_opts(text, Meta, CtxStyleOpts, StyleOpts),
-    render(S, SubHtml, StyleOpts),
-    format(string(Html), "<p class=\"text\">\n~w</p>\n", [SubHtml]). 
+    Depth1 is Depth+1,
+    render(S, SubHtml, StyleOpts, Depth1),
+    enclose_in_tags("p", Depth, SubHtml, Html).
 
-render(text(S), Html, CtxStyleOpts) :-
-    render(text(S, _{}), Html, CtxStyleOpts).
+render(text(S), Html, CtxStyleOpts, Depth) :-
+    render(text(S, _{}), Html, CtxStyleOpts, Depth).
 
-render(quote(S, Meta), Html, CtxStyleOpts) :-
+render(quote(S, Meta), Html, CtxStyleOpts, Depth) :-
     style_opts(quote, Meta, CtxStyleOpts, StyleOpts),
-    render(S, SubHtml, StyleOpts),
-    format(string(Html), "<a>\n~w</a>\n", [SubHtml]). 
+    Depth1 is Depth+1,
+    render(S, SubHtml, StyleOpts, Depth1),
+    enclose_in_tags("a", Depth, SubHtml, Html).
 
-render(quote(S), Html, CtxStyleOpts) :-
-    render(quote(S, _{}), Html, CtxStyleOpts).
+render(quote(S), Html, CtxStyleOpts, Depth) :-
+    render(quote(S, _{}), Html, CtxStyleOpts, Depth).
 
 %% Lists
-render([], "", _).
-render([H|T], HtmlBody, CtxStyleOpts) :-
-    render(H, Acc, CtxStyleOpts),
-    render(T, HtmlBody2, CtxStyleOpts),
+render([], "", _, _).
+render([H|T], HtmlBody, CtxStyleOpts, Depth) :-
+    render(H, Acc, CtxStyleOpts, Depth),
+    render(T, HtmlBody2, CtxStyleOpts, Depth),
     string_concat(Acc, HtmlBody2, HtmlBody).
 
 %% Basic strings
-render(S, SS, _) :- string(S), string_concat(S, "\n", SS).
+render(S, SS, _, _) :- string(S), string_concat(S, "\n", SS).
 
 %% Default is an error in the html
-render(S, E, _) :-
+render(S, E, _, _) :-
     term_string(S, SS),
     format(string(E), "Failed to render: ~w\n", [SS]).
 
