34db5f9c3358ac42ccbfbf9139f3993882ba3b57
jam <jwalkerdine21@hotmail.com>
2025-02-05 17:10:57 +0000

    Refactor to simplify rendering

--- src/core.pl
+++ src/core.pl
@@ -17,15 +17,15 @@ author("Jean Baudrillard", []).
 notes("Simulation and Simulacra",
       [
           h1("The Procession of Simulacra", _{}),
-          text([quote("The simulacrum is never what hides the truth -
+          p([a("The simulacrum is never what hides the truth -
                 it is truth that hides the fact that there is none. The
                 simulacrum is true."),
-                text("- Ecclesiastes", _{fontsize:16})],
-               _{tags:["christianity"], fontsize:14}),
-          text("Coextensivity of the real and its concept is imaginary"),
-          text("The real is produced from miniaturized cells, matrices, and 
+             a("- Ecclesiastes", _{fontsize:16})],
+               _{tags:["christianity"], fontsize:14, class: "text"}),
+          p("Coextensivity of the real and its concept is imaginary"),
+          p("The real is produced from miniaturized cells, matrices, and 
 memory banks, models of control - and it can be reproduced an indefinite number of times from these. It no longer needs to be rational, because it no longer measures itself against either an ideal or negative instance. It is no longer anything but operational."),
-          text("Interpretation-- The system is the point from which we produce 'reality', and as long as the system is functional, what happens in reality does not matter except in the same way that it might suck if a map is wrong"),
+          p("Interpretation-- The system is the point from which we produce 'reality', and as long as the system is functional, what happens in reality does not matter except in the same way that it might suck if a map is wrong"),
           h1("The Divine Irreverence of Images")
       ]).
 
@@ -34,6 +34,6 @@ book("Simultaneous Statistical Inference", _{tags:["maths"]}).
 notes("Simultaneous Statistical Inference",
       [
           h1("Intro"),
-          text("In simultaneous statistical inference, we have less hypotheses than data-sets, I.E., a singular hypothesis may talk about multiple datasets."),
-          text("This can mean that in multiple comparison, a decision is not only made between H_0 and not H_0, but also between all possible combinations")
+          p("In simultaneous statistical inference, we have less hypotheses than data-sets, I.E., a singular hypothesis may talk about multiple datasets."),
+          p("This can mean that in multiple comparison, a decision is not only made between H_0 and not H_0, but also between all possible combinations")
       ]).
--- src/render.pl
+++ src/render.pl
@@ -1,17 +1,47 @@
 :- module(render, [render/2, write_notes_to_html/1]).
 
-%% Global Styles
-style(h2,
-      _{fontsize:"18px", element: "h2"}).
-style(h1,
-      _{color:"red", inherits:h2, element: "h1"}).
-style(text,
-      _{fontsize:"14px", type:class, element: "p", classname: ".text"}).
-style(quote,
-      _{implements:text, fontstyles:[italics], element:"a", classname:".quote"}).
-style(img,
-      _{element:"img"}).
+%% CSS
 
+%%% Global Styles
+elem_style(h2, _{fontsize:"18px"}).
+elem_style(h1, _{color:"black", inherits:h2}).
+elem_style(img, _{element:"img"}).
+
+class_style(".text", _{fontsize:"14px"}).
+class_style(".quote", _{inherits:text, fontstyles:[italics]}).
+
+%%% Conversion of CSS names from atoms
+css_style_atom(fontsize-V, "font-size", V) :- !.
+css_style_atom(color-V, "color", V) :- !.
+
+%%% Rendering
+render_as_css_aux(K-V, Acc, AccNew) :-
+    css_style_atom(K-V, KS, VS),
+    format(string(AccNew), "~w  ~w: ~w;\n", [Acc, KS, VS]),
+    !.
+
+render_as_css_aux(_-_, Acc, Acc).
+
+render_as_css_aux_no_whitespace(K-V, Acc, AccNew) :-
+    css_style_atom(K-V, KS, VS),
+    format(string(AccNew), "~w~w: ~w;", [Acc, KS, VS]),
+    !.
+
+render_as_css_aux_no_whitespace(_-_, Acc, Acc).
+
+meta_to_style_string(Meta, StyleString) :-
+    dict_pairs(Meta, _, StylePairs),
+    foldl(render_as_css_aux, StylePairs, "", StyleString).    
+
+render_style_as_css(Name, Meta, CSS) :-
+    atom_string(Name, StyleName),
+    meta_to_style_string(Meta, StyleString),
+    format(string(CSS), "~w {\n~w}\n", [StyleName, StyleString]).
+
+%% HTML
+inline_css(Meta, StyleString) :-
+    dict_pairs(Meta, _, MetaPairs),
+    foldl(render_as_css_aux_no_whitespace, MetaPairs, "", StyleString).
 
 enclose_in_tags(Tag, Depth, Content, Result) :-
     format(string(Result),
@@ -28,48 +58,19 @@ enclose_in_tags(Tag, Class, StyleString, Depth, Content, Result) :-
            "~*c<~w class=\"~w\" style=\"~w\">\n~w~*c</~w>\n",
            [Depth, 32, Tag, Class, StyleString, Content, Depth, 32, Tag]).
 
-
-css_style_atom(fontsize-V, "font-size", V) :- !.
-css_style_atom(color-V, "color", V) :- !.
-
-render_as_css_aux(K-V, Acc, AccNew) :-
-    css_style_atom(K-V, KS, VS),
-    format(string(AccNew), "~w  ~w: ~w;\n", [Acc, KS, VS]),
-    !.
-
-render_as_css_aux(_-_, Acc, Acc).
-
-render_as_css_aux_no_whitespace(K-V, Acc, AccNew) :-
-    css_style_atom(K-V, KS, VS),
-    format(string(AccNew), "~w~w: ~w;", [Acc, KS, VS]),
-    !.
-
-render_as_css_aux_no_whitespace(_-_, Acc, Acc).
-
-meta_with_inherit(Meta, WithInheritedMeta) :-
-    get_dict(inherits, Meta, InheritedTag),
-    style(InheritedTag, InheritedMeta),
-    put_dict(Meta, InheritedMeta, WithInheritedMeta),
-    !.
-
-meta_with_inherit(Meta, Meta).
-
-meta_to_style_string(Meta, StyleString) :-
-    dict_pairs(Meta, _, StylePairs),
-    foldl(render_as_css_aux, StylePairs, "", StyleString).    
-
-render_style_as_css(Meta, CSS) :-
-    meta_with_inherit(Meta, WithInheritedMeta),
-    (get_dict(classname, WithInheritedMeta, StyleName), !;
-     get_dict(element, WithInheritedMeta, StyleName), !),
-    meta_to_style_string(WithInheritedMeta, StyleString),
-    format(string(CSS), "~w {\n~w}\n", [StyleName, StyleString]).
-
 html_header_aux(StyleList, StyleString) :-
     foldl(string_concat, StyleList, "", StyleString).
     
 html_header(HtmlHeader) :-
-    findall(CSS, (style(_, Meta), render_style_as_css(Meta, CSS)), StyleList),
+    findall(CSS,
+            (elem_style(Name, Meta),
+             render_style_as_css(Name, Meta, CSS)),
+            ElemStyleList),
+    findall(CSS,
+            (class_style(Name, Meta),
+             render_style_as_css(Name, Meta, CSS)),
+            ClassStyleList),
+    append(ClassStyleList, ElemStyleList, StyleList),
     html_header_aux(StyleList, StyleString),
     enclose_in_tags("style", 0, StyleString, HtmlHeader).
 
@@ -85,27 +86,32 @@ render(Name, Html) :-
            "<html>\n<head>\n~w</head>\n<body>\n~w</body>\n</html>",
            [HtmlHeader, HtmlBody]).
 
-inline_css(Meta, StyleString) :-
-    dict_pairs(Meta, _, MetaPairs),
-    foldl(render_as_css_aux_no_whitespace, MetaPairs, "", StyleString).
 
+%% Lists
+render([], "", _, _) :- !.
+render([H|T], HtmlBody, Ctx, Depth) :-
+    render(H, Acc, Ctx, Depth),
+    render(T, HtmlBody2, Ctx, Depth),
+    string_concat(Acc, HtmlBody2, HtmlBody),
+    !.
+
+% Nodes
 render(Term, Html, Ctx, Depth) :-
+    %% Find element, and the metadata
     compound(Term),
     compound_name_arguments(Term, P, [S, NodeMeta]),
-    
-    style(P, StyleMeta),
-    meta_with_inherit(StyleMeta, StyleMetaWithInherit),
-    get_dict(element, StyleMetaWithInherit, Element),
+    atom_string(P, Element),
 
+    %% Enrich meta with context, inline the styling into stylestring
     put_dict(NodeMeta, Ctx, CtxNew),
     inline_css(CtxNew, CSS),
-    
+
+    %% Render subnodes
     Depth1 is Depth+1,
     render(S, SubHtml, CtxNew, Depth1),
-    
-    (get_dict(classname, StyleMetaWithInherit, ClassNameWithDots) ->
-         split_string(ClassNameWithDots, ".", "", ClassNameSplitList),
-         last(ClassNameSplitList, ClassName),
+
+    %% Provide class, render
+    (get_dict(class, CtxNew, ClassName) ->
          (CSS = "" ->
               enclose_in_tags(Element, ClassName, Depth, SubHtml, Html), !;
           enclose_in_tags(Element, ClassName, CSS, Depth, SubHtml, Html), !);
@@ -117,14 +123,6 @@ render(Term, Html, Ctx, Depth) :-
     compound_name_arguments(Term, P, [S]),    
     compound_name_arguments(NewTerm, P, [S, _{}]),
     render(NewTerm, Html, Ctx, Depth),
-    !.
-
-%% Lists
-render([], "", _, _) :- !.
-render([H|T], HtmlBody, Ctx, Depth) :-
-    render(H, Acc, Ctx, Depth),
-    render(T, HtmlBody2, Ctx, Depth),
-    string_concat(Acc, HtmlBody2, HtmlBody),
     !.
 
 %% Basic strings
